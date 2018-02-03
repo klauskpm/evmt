afebbed14ee20f2c327a45cffb6ac53f1064b93f
Klaus Kazlauskas <klausmachado@gmail.com>
2018-02-03 18:41:31 -0200

    Creating a basic workflow to publish (#13)
    * NPM scripts changed for a better publishing process
    
    * Implemented the JS Standard as a linter
    
    * Setting precommit and prepush as NPM scripts
    
    * Added husky to handle git hooks
    Added ignore files for standard js
    Cleaned the lint NPM script command
    
    * Build file created with the min nomenclature
    
    * Lowered the threshold for real expectations on future collaborations

--- src/event-emitter-dom/event-emitter-dom.js
+++ src/event-emitter-dom/event-emitter-dom.js
@@ -1,43 +1,43 @@
 export class EventEmitterDOM {
-    constructor() {
-        this.subscriptions = [];
+  constructor () {
+    this.subscriptions = []
+  }
+
+  /**
+   * Emits the arguments to all subscribed callbacks
+   *
+   * @param args {any} How many and of type you want
+   */
+  emit (...args) {
+    this.subscriptions.forEach(subscription => {
+      subscription(...args)
+    })
+  }
+
+  /**
+   * Subscribes an callback to receive a future emit
+   *
+   * @param callback {Function} A callback
+   * @returns {Function}
+   */
+  subscribe (callback) {
+    const subscription = callback
+
+    subscription.remove = () => {
+      this.remove(this.subscriptions.indexOf(subscription))
     }
 
-    /**
-     * Emits the arguments to all subscribed callbacks
-     *
-     * @param args {any} How many and of type you want
-     */
-    emit(...args) {
-        this.subscriptions.forEach(subscription => {
-            subscription(...args);
-        });
-    }
+    this.subscriptions.push(subscription)
 
-    /**
-     * Subscribes an callback to receive a future emit
-     *
-     * @param callback {Function} A callback
-     * @returns {Function}
-     */
-    subscribe(callback) {
-        const subscription = callback;
+    return subscription
+  }
 
-        subscription.remove = () => {
-            this.remove(this.subscriptions.indexOf(subscription));
-        };
-
-        this.subscriptions.push(subscription);
-
-        return subscription;
-    }
-
-    /**
-     * Removes the subscribed callback
-     *
-     * @param index {Integer} The callback/subscription index
-     */
-    remove(index) {
-        this.subscriptions.splice(index, 1);
-    }
+  /**
+   * Removes the subscribed callback
+   *
+   * @param index {Integer} The callback/subscription index
+   */
+  remove (index) {
+    this.subscriptions.splice(index, 1)
+  }
 }
--- src/event-emitter-dom/event-emitter-dom.spec.js
+++ src/event-emitter-dom/event-emitter-dom.spec.js
@@ -1,90 +1,89 @@
-import { EventEmitterDOM } from './event-emitter-dom';
+import { EventEmitterDOM } from './event-emitter-dom'
 
-let onSelect,
-    subscription1,
-    subscription2,
-    subscriptionValue1 = null,
-    subscriptionValue2 = null;
+let onSelect
+let subscription1
+let subscription2
+let subscriptionValue1 = null
+let subscriptionValue2 = null
 
 const callbacks = {
-    subscription1: (firstArg) => {
-        subscriptionValue1 = firstArg;
-    },
-    subscription2: (firstArg, secondArg) => {
-        subscriptionValue2 = {[secondArg]: firstArg};
-    }
-};
+  subscription1: (firstArg) => {
+    subscriptionValue1 = firstArg
+  },
+  subscription2: (firstArg, secondArg) => {
+    subscriptionValue2 = {[secondArg]: firstArg}
+  }
+}
 
 const select = (...args) => {
-    onSelect.emit(...args);
-};
+  onSelect.emit(...args)
+}
 
 describe('EventEmitter', () => {
+  beforeEach(() => {
+    onSelect = new EventEmitterDOM()
+  })
 
+  it('should be defined', () => {
+    expect(EventEmitterDOM).toBeDefined()
+  })
+
+  it('should be instance of EventEmitterDOM', () => {
+    expect(onSelect instanceof EventEmitterDOM).toBe(true)
+    expect(onSelect.subscriptions.length).toBe(0)
+  })
+
+  it('should have a subscription', () => {
+    onSelect.subscribe(() => {})
+
+    expect(onSelect.subscriptions.length).toBe(1)
+  })
+
+  describe('after subscribing some callbacks', () => {
     beforeEach(() => {
-        onSelect = new EventEmitterDOM();
-    });
+      spyOn(callbacks, 'subscription1').and.callThrough()
+      spyOn(callbacks, 'subscription2').and.callThrough()
 
-    it('should be defined', () => {
-        expect(EventEmitterDOM).toBeDefined();
-    });
+      onSelect = new EventEmitterDOM()
+      subscriptionValue1 = null
+      subscriptionValue2 = null
 
-    it('should be instance of EventEmitterDOM', () => {
-        expect(onSelect instanceof EventEmitterDOM).toBe(true);
-        expect(onSelect.subscriptions.length).toBe(0);
-    });
+      subscription1 = onSelect.subscribe(callbacks.subscription1)
+      subscription2 = onSelect.subscribe(callbacks.subscription2)
+    })
 
-    it('should have a subscription', () => {
-        onSelect.subscribe(() => {});
+    it('should have subscriptions', () => {
+      expect(onSelect.subscriptions.length).toBe(2)
+    })
 
-        expect(onSelect.subscriptions.length).toBe(1);
-    });
+    it('should emit the subscribed callback', () => {
+      const firstArg = 'hi'
+      const secondArg = 'nail'
 
-    describe('after subscribing some callbacks', () => {
-        beforeEach(() => {
-            spyOn(callbacks, 'subscription1').and.callThrough();
-            spyOn(callbacks, 'subscription2').and.callThrough();
+      select(firstArg, secondArg)
 
-            onSelect = new EventEmitterDOM();
-            subscriptionValue1 = null;
-            subscriptionValue2 = null;
+      expect(callbacks.subscription1).toHaveBeenCalled()
+      expect(callbacks.subscription1).toHaveBeenCalledWith(firstArg, secondArg)
+      expect(subscriptionValue1).toBe(firstArg)
+      expect(callbacks.subscription2).toHaveBeenCalled()
+      expect(callbacks.subscription2).toHaveBeenCalledWith(firstArg, secondArg)
+      expect(JSON.stringify(subscriptionValue2)).toBe(JSON.stringify({[secondArg]: firstArg}))
+    })
 
-            subscription1 = onSelect.subscribe(callbacks.subscription1);
-            subscription2 = onSelect.subscribe(callbacks.subscription2);
-        });
+    it('should remove the subscriptions', () => {
+      const firstArg = 127
 
-        it('should have subscriptions', () => {
-            expect(onSelect.subscriptions.length).toBe(2);
-        });
+      spyOn(onSelect, 'remove').and.callThrough()
 
-        it('should emit the subscribed callback', () => {
-            const firstArg = 'hi';
-            const secondArg = 'nail';
+      subscription2.remove()
+      select(firstArg)
+      subscription1.remove()
+      select(firstArg)
 
-            select(firstArg, secondArg);
-
-            expect(callbacks.subscription1).toHaveBeenCalled();
-            expect(callbacks.subscription1).toHaveBeenCalledWith(firstArg, secondArg);
-            expect(subscriptionValue1).toBe(firstArg);
-            expect(callbacks.subscription2).toHaveBeenCalled();
-            expect(callbacks.subscription2).toHaveBeenCalledWith(firstArg, secondArg);
-            expect(JSON.stringify(subscriptionValue2)).toBe(JSON.stringify({[secondArg]: firstArg}));
-        });
-
-        it('should remove the subscriptions', () => {
-            const firstArg = 127;
-
-            spyOn(onSelect, 'remove').and.callThrough();
-
-            subscription2.remove();
-            select(firstArg);
-            subscription1.remove();
-            select(firstArg);
-
-            expect(onSelect.remove).toHaveBeenCalledTimes(2);
-            expect(callbacks.subscription1).toHaveBeenCalledTimes(1);
-            expect(callbacks.subscription1).toHaveBeenCalledWith(firstArg);
-            expect(onSelect.subscriptions.length).toBe(0);
-        });
-    });
-});
+      expect(onSelect.remove).toHaveBeenCalledTimes(2)
+      expect(callbacks.subscription1).toHaveBeenCalledTimes(1)
+      expect(callbacks.subscription1).toHaveBeenCalledWith(firstArg)
+      expect(onSelect.subscriptions.length).toBe(0)
+    })
+  })
+})
