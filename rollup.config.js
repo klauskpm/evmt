f7edad6d13d3107c402498aca5190b5fab80eca3
Klaus Kazlauskas <klausmachado@gmail.com>
2018-02-03 18:08:45 -0200

    Build file created with the min nomenclature

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
 
-const production = !process.env.ROLLUP_WATCH
+// const production = !process.env.ROLLUP_WATCH
 
 export default {
   input: 'index.js',
   output: {
-    file: 'dist/index.js',
+    file: 'dist/index.min.js',
     format: 'cjs', // immediately-invoked function expression — suitable for <script> tags
     sourcemap: true
   },
@@ -14,6 +14,6 @@ export default {
     babel({
       exclude: 'node_modules/**'
     }),
-    production && uglify() // minify, but only in production
+    uglify() // minify, but only in production
   ]
 }
